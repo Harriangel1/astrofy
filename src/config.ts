6d1b1363e23b897f32c86ff67e7fb7a94e28aa6a
Harriangel1 <harrietjombo@gmail.com>
2024-11-26 11:42:19 +0000

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astrofy | Personal Portfolio Website Template';
-export const SITE_DESCRIPTION = 'Astrofy is a free and open-source template for your Personal Portfolio Website built with Astro and TailwindCSS. Create in minutes a website with Blog, CV, Project Section, Store and RSS Feed.';
+export const SITE_TITLE = 'Harriet Jombo | Digital Marketing Expert';
+export const SITE_DESCRIPTION = 'Harriet is your go to person on anything SEO Optimization, email marketing and lead generation. Create in minutes a website with Blog, CV, Project Section, Store and RSS Feed.';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
